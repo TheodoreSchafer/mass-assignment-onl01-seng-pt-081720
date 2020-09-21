a4e9202f2a19a3a601db6c3df94ef104d2e38571
Theodore Schafer <theodore.schafer@gmail.com>
2020-09-21 14:56:39 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,7 @@
 class Person
-  attr_accessor :name, :birthday, :hair_color, :eye_color, :weight, :height
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, 
+:weight, :handed, :complexion, :t_shirt_size, 
+:wrist_size, :glove_size, :pant_length, :pant_width
 
   def initialize(attribute)
     attribute.each do |key, value|
