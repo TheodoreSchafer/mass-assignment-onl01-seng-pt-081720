f155fb937e5cf359cb02e82b2a3be79f588018ea
Theodore Schafer <theodore.schafer@gmail.com>
2020-09-21 14:59:31 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,8 +3,8 @@ class Person
 :weight, :handed, :complexion, :t_shirt_size,
 :wrist_size, :glove_size, :pant_length, :pant_width
 
-  def initialize(attribute)
-    attribute.each do {|key, value| self.send(("#{key}"), value)}
+  def initialize(attributes)
+    attribute.each do
   end
 
 
