291a78d0e3dd82dbe2db35cfa2cc01961d6dceca
Theodore Schafer <theodore.schafer@gmail.com>
2020-09-21 14:54:32 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,7 +2,7 @@ class Person
   attr_accessor :name, :birthday, :hair_color, :eye_color, :weight, :height
 
   def initialize(attribute)
-    attributes.each do |key, value|
+    attribute.each do |key, value|
       self.send(("#{key}"), value)
     end
   end
