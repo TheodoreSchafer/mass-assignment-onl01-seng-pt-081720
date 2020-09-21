8b9eec48877f6ec69727d1b6a5219baf3dd751f0
Theodore Schafer <theodore.schafer@gmail.com>
2020-09-21 14:50:38 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,6 +2,10 @@ class Person
   attr_accessor :name, :birthday, :hair_color, :eye_color, :weight, :height
 
   def initialize
+    @name = name
+    @birthday = birthday
+    @hair_color = hair_color
+    
   end
   #your code here
 end
